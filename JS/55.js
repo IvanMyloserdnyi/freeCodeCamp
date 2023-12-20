301d671dc1d436f0b08afd30f45cf558245c33bd
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-12-20 16:17:41 +0200

    Create 55.js

---
+++ JS/55.js
@@ -0,0 +1,15 @@
+// Налаштування
+let sum = 0;
+
+function addThree() {
+    sum += 3;
+}
+
+// Змініть код лише під цим рядком
+function addFive() {
+    sum +=5
+}
+// Змініть код лише над цим рядком
+
+addThree();
+addFive();
